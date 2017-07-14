b80a4db67204507aa9030517835226e6b548bbd5
MathieuAuclair <mathiass12@hotmail.com>
2017-07-14 11:37:03 -0400

    fix the switch issue

--- main.cpp
+++ main.cpp
@@ -7,193 +7,14 @@ PwmOut b(p24);   //p24 rigado p15 beacon
 uint16_t rgbUUID  = 0xA000;
 uint16_t UUID     = 0xA001;
 
-const static char     DEVICE_NAME[]        = "ISOLIGHT"; 
-
+const static char DEVICE_NAME[] = "ISOLIGHT"; 
 
 static uint8_t Value[10] = {0};
 WriteOnlyArrayGattCharacteristic<uint8_t, sizeof(Value)> Char(UUID, Value);
 
-
-BLEDevice           ble;
+BLEDevice ble;
 GattCharacteristic *characteristics[] = {&Char};
-GattService        rgbService(rgbUUID, characteristics, sizeof(characteristics) / sizeof(GattCharacteristic *));
-
-
-void disconnectionCallback(const Gap::DisconnectionCallbackParams_t *)
-{
-    ble.gap().startAdvertising();
-    r.write(0.0);   //(0.0) pour rigado (1.0) pour beacon
-    g.write(0.0);   //(0.0) pour rigado (1.0) pour beacon
-    b.write(0.0);   //(0.0) pour rigado (1.0) pour beacon
-}
-
-
-void writeCharCallback(const GattWriteCallbackParams *params){
-    if(params->handle == Char.getValueHandle()) {
-            if ( *(params->data) == 00 ){
-                r = 0/255.0f;
-                g = 0/255.0f;
-                b = 0/255.0f;
-            }
-            else if ( *(params->data) == 01 ){
-                r = 255/255.0f;
-                g = 255/255.0f;
-                b = 255/255.0f;
-            }               
-            else if ( *(params->data) == 02 ){
-                r = 255/255.0f;
-                g = 204/255.0f;
-                b = 204/255.0f;
-            }
-            else if ( *(params->data) == 03 ){
-                r = 255/255.0f;
-                g = 204/255.0f;
-                b = 153/255.0f;
-            } 
-            else if ( *(params->data) == 04 ){
-                r = 255/255.0f;
-                g = 255/255.0f;
-                b = 153/255.0f;
-            }               
-            else if ( *(params->data) == 05 ){
-                r = 153/255.0f;
-                g = 255/255.0f;
-                b = 153/255.0f;
-            }
-            else if ( *(params->data) == 06 ){
-                r = 153/255.0f;
-                g = 255/255.0f;
-                b = 255/255.0f;
-            } 
-            else if ( *(params->data) == 07 ){
-                r = 204/255.0f;
-                g = 204/255.0f;
-                b = 255/255.0f;
-            }               
-            else if ( *(params->data) == 38 ){ //8 ne fonctionne pas, pourquoi?
-                r = 255/255.0f;
-                g = 153/255.0f;
-                b = 255/255.0f;
-            }
-            else if ( *(params->data) == 39 ){ //9 ne fonctionne pas, pourquoi?
-                r = 255/255.0f;
-                g = 102/255.0f;
-                b = 102/255.0f;
-            } 
-            else if ( *(params->data) == 10 ){
-                r = 255/255.0f;
-                g = 153/255.0f;
-                b = 102/255.0f;
-            }               
-            else if ( *(params->data) == 11 ){
-                r = 255/255.0f;
-                g = 255/255.0f;
-                b = 102/255.0f;
-            }
-            else if ( *(params->data) == 12 ){
-                r = 255/255.0f;
-                g = 255/255.0f;
-                b = 51/255.0f;
-            } 
-            else if ( *(params->data) == 13 ){
-                r = 51/255.0f;
-                g = 255/255.0f;
-                b = 51/255.0f;
-            }               
-            else if ( *(params->data) == 14 ){
-                r = 0/255.0f;
-                g = 255/255.0f;
-                b = 255/255.0f;
-            }
-            else if ( *(params->data) == 15 ){
-                r = 153/255.0f;
-                g = 153/255.0f;
-                b = 255/255.0f;
-            } 
-            else if ( *(params->data) == 16 ){
-                r = 255/255.0f;
-                g = 0/255.0f;
-                b = 255/255.0f;
-            }               
-            else if ( *(params->data) == 17 ){
-                r = 255/255.0f;
-                g = 0/255.0f;
-                b = 0/255.0f;
-            }
-            else if ( *(params->data) == 18 ){
-                r = 255/255.0f;
-                g = 153/255.0f;
-                b = 0/255.0f;
-            } 
-            else if ( *(params->data) == 19 ){
-                r = 255/255.0f;
-                g = 255/255.0f;
-                b = 0/255.0f;
-            }               
-            else if ( *(params->data) == 20 ){
-                r = 0/255.0f;
-                g = 255/255.0f;
-                b = 0/255.0f;
-            }
-            else if ( *(params->data) == 21 ){
-                r = 51/255.0f;
-                g = 102/255.0f;
-                b = 255/255.0f;
-            } 
-            else if ( *(params->data) == 22 ){
-                r = 102/255.0f;
-                g = 102/255.0f;
-                b = 204/255.0f;
-            }
-            else if ( *(params->data) == 23 ){
-                r = 204/255.0f;
-                g = 102/255.0f;
-                b = 204/255.0f;
-            } 
-            else if ( *(params->data) == 24 ){
-                r = 255/255.0f;
-                g = 102/255.0f;
-                b = 0/255.0f;
-            }
-            else if ( *(params->data) == 25 ){
-                r = 255/255.0f;
-                g = 204/255.0f;
-                b = 0/255.0f;
-            } 
-            else if ( *(params->data) == 26 ){
-                r = 51/255.0f;
-                g = 204/255.0f;
-                b = 0/255.0f;
-            }
-            else if ( *(params->data) == 27 ){
-                r = 0/255.0f;
-                g = 204/255.0f;
-                b = 204/255.0f;
-            } 
-            else if ( *(params->data) == 28 ){
-                r = 0/255.0f;
-                g = 0/255.0f;
-                b = 255/255.0f;
-            }
-            else if ( *(params->data) == 29 ){
-                r = 102/255.0f;
-                g = 0/255.0f;
-                b = 204/255.0f;
-            } 
-            else if ( *(params->data) == 30 ){
-                r = 153/255.0f;
-                g = 51/255.0f;
-                b = 153/255.0f;
-            } 
-            else{
-                r = 0/255.0f;
-                g = 0/255.0f;
-                b = 0/255.0f;
-		//create a log over here sigh!
-            }
-      }           
-}
-
+GattService rgbService(rgbUUID, characteristics, sizeof(characteristics) / sizeof(GattCharacteristic *));
 
 void bleInitComplete(BLE::InitializationCompleteCallbackContext *params)//init connection with phone
 {
@@ -203,8 +24,6 @@ void bleInitComplete(BLE::InitializationCompleteCallbackContext *params)//init c
     if (error != BLE_ERROR_NONE) {
         return;
     }
-
-
 
     ble.init();
     ble.gap().onDisconnection(disconnectionCallback);
@@ -222,8 +41,8 @@ void bleInitComplete(BLE::InitializationCompleteCallbackContext *params)//init c
 
     /* Start advertising */
     ble.gap().startAdvertising();
-
 }    
+
 int main(void)
 {
     BLE& ble = BLE::Instance(BLE::DEFAULT_INSTANCE);
@@ -238,3 +57,123 @@ int main(void)
         ble.waitForEvent();
     }
 }
+
+void disconnectionCallback(const Gap::DisconnectionCallbackParams_t *)
+{
+    ble.gap().startAdvertising();
+    r.write(0.0);   //(0.0) pour rigado (1.0) pour beacon
+    g.write(0.0);   //(0.0) pour rigado (1.0) pour beacon
+    b.write(0.0);   //(0.0) pour rigado (1.0) pour beacon
+}
+
+void changeLedRgbIndexValues(float red, float green, float blue){
+	r = red/255.0f;
+	g = green/255.0f;
+	b = blue/255.0f;
+}
+
+void writeCharCallback(const GattWriteCallbackParams *params){
+    if(params->handle == Char.getValueHandle()) {
+            switch(*(params->data)){
+		case 00:
+                	changeLedRgbIndexValues(0,0,0);
+            	break;
+            	case 01:
+			changeLedRgbIndexValues(255,255,255);
+            	break;    
+            	case 02: 
+			changeLedRgbIndexValues(255,204,204);
+            	break;
+            	case 03: 
+			changeLedRgbIndexValues(255,204,153);
+            	break;
+            	case 04: 
+			changeLedRgbIndexValues(255,255,153);
+            	break;   
+            	case 05: 
+			changeLedRgbIndexValues(153,255,153);
+            	break;
+            	case 06:
+			changeLedRgbIndexValues(153,255,255);
+            	break;
+            	case 07: 
+			changeLedRgbIndexValues(204,204,255);
+            	break;   
+            	case 38: //8 ne fonctionne pas, pourquoi?
+			changeLedRgbIndexValues(255,153,255);
+            	break
+            	case 39:  //9 ne fonctionne pas, pourquoi?
+			changeLedRgbIndexValues(255,102,102);
+            	break;
+            	case 10: 
+			changeLedRgbIndexValues(255,153,102);
+            	break;   
+            	case 11:
+			changeLedRgbIndexValues(255,255,102);
+            	break;
+            	case 12: 
+			changeLedRgbIndexValues(255,255,51);
+            	break;
+           	case 13: 
+			changeLedRgbIndexValues(51,255,51);
+            	break;   
+            	case 14:
+			changeLedRgbIndexValues(0,255,255);
+            	break;
+            	case 15:
+			changeLedRgbIndexValues(153,153,255);
+            	break;
+            	case 16:
+			changeLedRgbIndexValues(255,0,255);
+            	break;	
+            	case 17:
+			changeLedRgbIndexValues(255,0,0);
+           	break;
+            	case 18:
+			changeLedRgbIndexValues(255,153,0);
+            	break;
+            	case 19:
+			changeLedRgbIndexValues(255,255,0);
+            	break;    
+            	case 20:
+			changeLedRgbIndexValues(0,255,0);
+           	break;
+            	case 21: 
+			changeLedRgbIndexValues(51,102,255);
+            	break;
+            	case 22: 
+			changeLedRgbIndexValues(102,102,204);
+            	break;
+            	case 23: 
+			changeLedRgbIndexValues(204,102,204);
+            	break;
+            	case 24:
+			changeLedRgbIndexValues(255,102,0);
+            	break;
+            	case 25: 
+			changeLedRgbIndexValues(255,204,0);
+            	break;
+            	case 26: 
+			changeLedRgbIndexValues(51,204,0);
+            	break;
+            	case 27:
+			changeLedRgbIndexValues(0,204,204);
+            	break;
+            	case 28:
+			changeLedRgbIndexValues(0,0,255);
+            	break;
+            	case 29: 
+			changeLedRgbIndexValues(102,0,204);
+            	break;
+            	case 30: 
+			changeLedRgbIndexValues(153,51,153);
+            	break;
+            	default:
+			//create a log over here sigh!
+		break;
+            }
+      }           
+}
+
+
+
